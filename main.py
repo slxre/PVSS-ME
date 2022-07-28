e3d2621a41115f550cc45320ed3a71af041a214a
sʟxʀᴇ <slxre@pm.me>
2022-07-28 13:25:09 +0000

    import, script, & update

---
+++ main.py
@@ -0,0 +1,95 @@
+import time
+
+
+# QD
+user = 'member', 'Agent', 'SLxRE'
+MAC = 'gnrlaccess', 'Xi004', 'SLxREx'
+UEC = 'egenrlacces', '400iX', 'SLxRExx'
+exitt = 'E'
+mact1 = True
+AAC = 'SLxRExx'
+AEC = 'xxSLxRE'
+inputt2 = 'r'
+
+
+print("WELCOME 2 SLxRPvSS")
+time.sleep(2.5)
+print("Who Are You?: ")
+
+while(True):
+    continue 
+if user == "member".lower():
+    print(input("Enter Your MAC to Continue: "))
+    if MAC == 'gnrlaccess':
+        print("Welcome, Peasant")
+        time.sleep(3)
+        print("You Are Not Recognized By The Board and Our Leaders")
+        time.sleep(4)
+        print("SLxRiVN Registry Costs $100,000,000")
+        time.sleep(2)
+        print("The Bank is Curretly Closed Until The Next Election. See You Then.")
+        time.sleep(8)
+        print(input("Type E to Exit: "))
+        if exitt == 'E.'.lower():
+            print(input("Enter Your UEC Code to Exit "))
+            time.sleep(2)
+            if UEC == ['egnrlaccess', '400iX', 'SLxRExx']:
+                print("Goodbye Peasant, See You Next Year!!")
+                time.sleep(1)
+                quit()
+if user == 'SLxRE':
+    print(input("Enter Your AAC to Continue: "))
+    if AAC == 'SLxRExx':
+        print("Welcome, Admin")
+        time.sleep(3)
+        print("There Are No New SLxRIVNs That Have Recently Joined. Check Back Later. ")
+        time.sleep(4)
+        print("You Have Conquered The World. + 100% ")
+        time.sleep(5)
+        print("NEW MEMNER HAS JOINED THE WORLD. + 100% ")
+        time.sleep(1)
+        print("NEW MEMNER HAS JOINED THE WORLD. + 100% ")
+        time.sleep(1)
+        print("NEW MEMNER HAS JOINED THE WORLD. + 100% ")
+        time.sleep(1)
+        print("NEW MEMNER HAS JOINED THE WORLD. + 100% ")
+        time.sleep(1)
+        print("NEW MEMNER HAS JOINED THE WORLD. + 100% ")
+        time.sleep(1)
+        print(input("Press E to Exit"))
+        if e == 'E'.lower():
+            print(input("Enter Your AA Code to Exit "))
+            time.sleep(2)
+            if AEC == 'xxSLxRE':
+                print("Goodbye Agent. See You Later!")
+                time.sleep(2)
+                quit()
+    else:
+                    print("DENiED!!")
+                    time.sleep(1.5)
+                    print("Try Again Some Other Time")
+                    quit()
+else:
+                         print("Not A Valid User.")
+                         time.sleep(2)
+                         print("Try Again Some Other Time.")
+                         quit()
+
+# DATABASE / iNFO
+#_________________#
+# INPUTs
+user = ['member', 'Agent', 'SLxRE'] # Input for initial question ("Who Are You?: ")
+inputt2 = 'r'
+MAC = ['gnrlaccess', 'Xi004'] #Member Access Code
+UEC = ['egnrlaccess', '400iX'] #Universal Exit Code
+AAC = 'SLxRExx'  # "Admin Access Code"
+AEC = 'xxSLxRE' # Admin Exit Code
+
+# EXECUTABLEs
+exitt = 'E' # Executable to exit application
+mact1 = True
+# VERIFIERs
+veriT= True
+veriF = False
+
+
